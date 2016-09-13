4a863e44129b4dd093f11ff143a20b9901a549d8
Aram Drevekenin <grimsniffer@gmail.com>
2016-09-13 19:22:12 +0300

    refactor: clean up tests

--- test/unit/terminal-window.js
+++ test/unit/terminal-window.js
@@ -4,20 +4,22 @@ const fs = require('fs')
 
 const test = require('tape')
 
-const app = new Application({
-  path: electron,
-  args: [ `${__dirname}/../runner` ]
-})
+async function createApp () {
+  const app = new Application({
+    path: electron,
+    args: [ `${__dirname}/../runner` ]
+  })
+  await app.start()
+  await app.client.waitUntilWindowLoaded()
+  await app.browserWindow.isVisible()
+  await new Promise((resolve) => setTimeout(resolve, 500)) // allow pty time to load
+  return app
+}
 
 test('can create terminal window', async t => {
   t.plan(1)
   try {
-    await app.start()
-    await app.client.waitUntilWindowLoaded()
-    await app.browserWindow.isVisible()
-    await new Promise((resolve, reject) => {
-      setTimeout(resolve, 500) // allow pty time to load
-    })
+    const app = await createApp()
     const capturedBuf = await app.browserWindow.capturePage()
     const truth = fs.readFileSync(`${__dirname}/../screenshots/term-initial.png`)
     const matchesScreenshot = Buffer.compare(capturedBuf, truth) === 0
@@ -32,16 +34,9 @@ test('can create terminal window', async t => {
 test('can resize terminal window', async t => {
   t.plan(1)
   try {
-    await app.start()
-    await app.client.waitUntilWindowLoaded()
-    await app.browserWindow.isVisible()
-    await new Promise((resolve, reject) => {
-      setTimeout(resolve, 500) // allow pty time to load
-    })
+    const app = await createApp()
     await app.browserWindow.setSize(200, 200)
-    await new Promise((resolve, reject) => {
-      setTimeout(resolve, 100) // allow time to resize
-    })
+    await new Promise((resolve) => setTimeout(resolve, 100)) // allow time to resize
     const capturedBuf = await app.browserWindow.capturePage()
     const truth = fs.readFileSync(`${__dirname}/../screenshots/resizedWindow.png`)
     const matchesScreenshot = Buffer.compare(capturedBuf, truth) === 0
