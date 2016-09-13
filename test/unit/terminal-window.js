4d2496a6ee5d7ad9aa6b6b34a89d156e4a02d982
Aram Drevekenin <grimsniffer@gmail.com>
2016-09-13 13:47:26 +0300

    test: terminal resize

--- test/unit/terminal-window.js
+++ test/unit/terminal-window.js
@@ -4,13 +4,14 @@ const fs = require('fs')
 
 const test = require('tape')
 
+const app = new Application({
+  path: electron,
+  args: [ `${__dirname}/../runner` ]
+})
+
 test('can create terminal window', async t => {
   t.plan(1)
   try {
-    const app = new Application({
-      path: electron,
-      args: [ `${__dirname}/../runner` ]
-    })
     await app.start()
     await app.client.waitUntilWindowLoaded()
     await app.browserWindow.isVisible()
@@ -20,10 +21,34 @@ test('can create terminal window', async t => {
     const capturedBuf = await app.browserWindow.capturePage()
     const truth = fs.readFileSync(`${__dirname}/../screenshots/term-initial.png`)
     const matchesScreenshot = Buffer.compare(capturedBuf, truth) === 0
+    await app.stop()
     t.ok(matchesScreenshot, 'terminal window appears as expected')
-    app.stop()
   } catch (e) {
+    await app.stop()
     t.fail(e)
-    app.stop()
+  }
+})
+
+test('can resize terminal window', async t => {
+  t.plan(1)
+  try {
+    await app.start()
+    await app.client.waitUntilWindowLoaded()
+    await app.browserWindow.isVisible()
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, 500) // allow pty time to load
+    })
+    await app.browserWindow.setSize(200, 200)
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, 100) // allow time to resize
+    })
+    const capturedBuf = await app.browserWindow.capturePage()
+    const truth = fs.readFileSync(`${__dirname}/../screenshots/resizedWindow.png`)
+    const matchesScreenshot = Buffer.compare(capturedBuf, truth) === 0
+    await app.stop()
+    t.ok(matchesScreenshot, 'terminal resized as expected')
+  } catch (e) {
+    await app.stop()
+    t.fail(e)
   }
 })
