7576007f3ca7dfaa4e86fd96b2c008441f2e543e
Aram Drevekenin <grimsniffer@gmail.com>
2016-09-23 17:19:46 +0300

    fix: multiple testing environments

--- test/visual/terminal-window.js
+++ test/visual/terminal-window.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const createApp = require('../util/create-app')
 const test = require('tape')
+const env = process.env.ENVNAME || 'linux'
 
 test('can create terminal window', async t => {
   t.plan(1)
@@ -13,7 +14,7 @@ test('can create terminal window', async t => {
     await new Promise((resolve) => setTimeout(resolve, 100))
     // allow time for new terminal line
     const capturedBuf = await app.browserWindow.capturePage()
-    const truth = fs.readFileSync(`${__dirname}/../screenshots/travis-term-initial.png`)
+    const truth = fs.readFileSync(`${__dirname}/../screenshots/${env}/term-initial.png`)
     const matchesScreenshot = Buffer.compare(capturedBuf, truth) === 0
     await app.stop()
     t.ok(matchesScreenshot, 'terminal window appears as expected')
@@ -30,8 +31,7 @@ test('can resize terminal window', async t => {
     await app.browserWindow.setSize(200, 200)
     await new Promise((resolve) => setTimeout(resolve, 100)) // allow time to resize
     const capturedBuf = await app.browserWindow.capturePage()
-    fs.writeFileSync(`${__dirname}/../screenshots/debug.png`, capturedBuf)
-    const truth = fs.readFileSync(`${__dirname}/../screenshots/travis-resizedWindow.png`)
+    const truth = fs.readFileSync(`${__dirname}/../screenshots/${env}/resizedWindow.png`)
     const matchesScreenshot = Buffer.compare(capturedBuf, truth) === 0
     await app.stop()
     t.ok(matchesScreenshot, 'terminal resized as expected')
