d56610cfacb418884833cca4040ad62dce56f8eb
DoronTsur <doront@wix.com>
2016-10-21 22:14:58 +0300

    chore: fix stupid mistakes, no ;

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ module.exports = function TerminalWindow (opts = {}) {
       window.document.getElementById('terminal-container')
     )`
   )
-  win.openDevTools();
   win.show()
   return win
 }
