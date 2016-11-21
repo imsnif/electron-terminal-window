0993e77db3e3b63489e15a6d3b571d6be9ac29c1
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-21 22:48:36 +0200

    chore: test structure

--- test/util/require-hook.js
+++ test/util/require-hook.js
@@ -2,7 +2,7 @@
 const istanbul = require('istanbul')
 const hook = istanbul.hook
 const instrumenter = new istanbul.Instrumenter()
-const myMatcher = function (file) { return !/node_modules/g.test(file) }
+const myMatcher = function (file) { return !/node_modules|register-copy/g.test(file) } // temporary until we get rid of this horrid hack
 const myTransformer = function (code, file) { return instrumenter.instrumentSync(code.toString(), file) }
 
 hook.hookRequire(myMatcher, myTransformer)
